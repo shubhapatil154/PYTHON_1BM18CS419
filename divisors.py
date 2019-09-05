86d3fbfbd9606757adb169bf17b2d05a297781e9
shubhapatil154 <53892562+shubhapatil154@users.noreply.github.com>
2019-09-05 11:37:35 +0530

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,8 +1,9 @@
 def divisors(num):
+    print(f'DIVISORS OF THE NUMBER {num} is')
     for  i in range(1,num+1):
         if(num%i==0):
             print(i)
 
 
-num=int(input('ENTER THE NUMBER\n'))
+num=int(input('ENTER THE NUMBER \n'))
 divisors(num)
